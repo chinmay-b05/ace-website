e55aa33cd8aff390ff9a625e50d947c609aeae3d
len-mendonca <lenmendonca2003@gmail.com>
2024-10-03 12:49:22 +0530

    feat: netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import auth from 'auth-astro';
 import icon from 'astro-icon';
-
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [tailwind(), react(), icon({}), auth()],
   output: 'server',
-  adapter: vercel(),
-
+  adapter: netlify(),
 });
