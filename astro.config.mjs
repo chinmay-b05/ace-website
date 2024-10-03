9a67adae079605224fbd73124c5cb4708f1d4662
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-10-03 18:23:13 +0530

    minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import auth from 'auth-astro';
 import icon from 'astro-icon';
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [tailwind(), react(), icon({}), auth()],
   output: 'server',
-  adapter: netlify(),
+  adapter: vercel(),
 });
