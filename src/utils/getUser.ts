43c7cd32e4b12c00057702449553c8ad4ef0b972
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-10-04 21:58:35 +0530

    fix: get user runtime error

--- src/utils/getUser.ts
+++ src/utils/getUser.ts
@@ -1,15 +1,9 @@
-import { db } from "@lib/db";
-import { userTable } from "@lib/db/schema";
+import { db } from '@lib/db';
+import { userTable } from '@lib/db/schema';
 import { eq } from 'drizzle-orm';
 
 export async function getUser(id: string) {
-    try {
-      const user = await db.query.userTable.findFirst({
-        where : eq(userTable.id , id as string)
-      })
-      return user
-    } catch (error) {
-      console.error('Error fetching user:', error);
-      return null;
-    }
-  }
+  return await db.query.userTable.findFirst({
+    where: eq(userTable.id, id),
+  });
+}
